7b139faf544ba771ac4880eedf747c85246ac6b7
Aravind Amruth madanu <vulcan5064@gmail.com>
2025-09-22 13:39:22 +0100

    Add files via upload

---
+++ python_fundamentals.py
@@ -0,0 +1,25 @@
+# day 1 : python basics
+# topic: Variables, data types , Operators, Type conversion
+
+#variables and constants
+age= 25         #int
+name= "arvi"    # string
+PI = 3.14159     # constant by convention
+
+#Arthimentic operators
+x=10
+y=3
+print("Addition:", x+y)
+print("floor Division:",x//y)
+
+#comparion operators
+print("Is x equal to y?", x==y)
+
+#Logical operators
+is_student = True
+print("Is student and x>y?", is_student and x>y)
+
+#Type Conversion
+num_str = "10"
+num_int = int(num_str)
+print("Converted string to int:", num_int)
